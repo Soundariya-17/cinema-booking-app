60188583c03233bab608448b67d08e371ad66685
Soundariya lakshmi venkatesh <125112386+Soundariya-17@users.noreply.github.com>
2025-07-31 17:25:40 +0530

    Create main App component

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to the Cinema Booking App</h1>
+            {/* Add your components here */}
+        </div>
+    );
+}
+
+export default App;
